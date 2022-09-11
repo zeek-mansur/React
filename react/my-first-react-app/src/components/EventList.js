bed70b3fb64e541c9950cfcc85d0adfe93053de2
Zeek <mzulkiful@yahoo.com>
2022-09-11 21:10:23 +0100

    CSS Modules

--- react/my-first-react-app/src/components/EventList.js
+++ react/my-first-react-app/src/components/EventList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useState} from 'react'
+import styles from './EventList.module.css'
 
 
 
@@ -7,9 +8,10 @@ export default function EventList({events, handleClick}){
 
     return( 
         events.map((event, index)=> (
-            <React.Fragment key = {event.id}>
+            <div className={styles.card} 
+                 key = {event.id}>
               <h2>{index} - {event.title}</h2>
               <button onClick = {()=> handleClick(event.id)}>delete event</button>
-            </React.Fragment>)
+            </div>)
     ))
 }
--- react/my-first-react-app/src/components/Title.js
+++ react/my-first-react-app/src/components/Title.js
@@ -3,7 +3,7 @@ import './Title.css'
 export default function Title({title, subtitle}){
 
     return(
-        <div className="title-block">
+        <div className="title-block card ">
             <h1 className="title">{title}</h1>
             <br />
             <h1 className="subtitle">{subtitle}</h1>
