682b407821a3c6a3ca67ec820cfe3853ad545ac8
Zeek <mzulkiful@yahoo.com>
2022-09-11 20:39:18 +0100

    Component Stylesheets

--- react/my-first-react-app/src/components/Title.js
+++ react/my-first-react-app/src/components/Title.js
@@ -1,10 +1,12 @@
+import './Title.css'
+
 export default function Title({title, subtitle}){
 
     return(
-        <>
+        <div className="title-block">
             <h1 className="title">{title}</h1>
             <br />
             <h1 className="subtitle">{subtitle}</h1>
-        </>
+        </div>
     )
 }
