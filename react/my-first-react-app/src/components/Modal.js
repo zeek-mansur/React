0fbecf6c5a6d5e498517283b106d5a958d647fd6
Zeek <mzulkiful@yahoo.com>
2022-09-11 20:45:10 +0100

    Inline styles

--- react/my-first-react-app/src/components/Modal.js
+++ react/my-first-react-app/src/components/Modal.js
@@ -1,11 +1,14 @@
 import ReactDOM  from 'react-dom'
-import React, { Children } from 'react'
 import './Modal.css'
 
 export default function Modal({children, handleClose}) {
   return ReactDOM.createPortal((
     <div className='modal-backdrop'>
-      <div className='modal'>
+      <div className='modal' style={{
+        border: "4px solid", 
+        borderColor: "#ff4500",
+        textAlign: "center"
+        }}>
         {children}
         <br/>
         <button onClick={handleClose}>close</button>
