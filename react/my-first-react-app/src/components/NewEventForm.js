f8fec6739f5e380af9501947093882d728addbd5
Zeek <mzulkiful@yahoo.com>
2022-09-12 20:41:08 +0100

    The useRef Hook

--- react/my-first-react-app/src/components/NewEventForm.js
+++ react/my-first-react-app/src/components/NewEventForm.js
@@ -1,29 +1,37 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './NewEventForm.css'
 
 export default function NewEventForm({addEvent}) {
 
-    const [title, setTitle] = useState('')
-    const [date, setDate]  = useState('')
+    // const [title, setTitle] = useState('')
+    // const [date, setDate]  = useState('')
+    const title = useRef()
+    const date = useRef()
+      
 
-    const resetFrom = ()  => {
+    
 
-        setTitle('')
-        setDate('')
+    const resetFrom = ()  => { 
+
+        title.current.value=('')
+        date.current.value=('')
     }
 
     const handleSubmit = (e) => {
 
         e.preventDefault()
+        console.log(title, date)
+
         const event = {
-            title: title,
-            date: date, 
+            title: title.current.value,
+            date: date.current.value, 
             id: Math.floor(Math.random()*10000)
         }
 
         addEvent(event)
         resetFrom()
-    }
+    
+}
 
     return (
         
@@ -31,15 +39,15 @@ export default function NewEventForm({addEvent}) {
             <label>
                <span> Event Title:</span>
             <input type='text' 
-            onChange={(e) => setTitle(e.target.value)} 
-            value={title}/>
+            ref={title}
+            />
             </label>
 
             <label>
                 <span> Event Date:</span>
                 <input type='date' 
-                onChange={(e)=> setDate(e.target.value)}
-                value={date}/>
+                ref={date}
+                />
             </label>
             <button>Submit</button>
         </form>
